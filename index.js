27bd273c938d0b4c46e4be1f4faa9dff5d87cf13
DeeBest <simphiwedladla8@gmail.com>
2024-04-28 14:27:00 +0200

    created a function to toggle the active class on the search bar container

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const footerParagraph = document.getElementById('footer-paragraph');
-const currentDate = new Date().getFullYear();
-footerParagraph.textContent = `Created by \xA9Simphiwe ${currentDate}.`;
+const currentYear = new Date().getFullYear();
+footerParagraph.textContent = `Created by \xA9Simphiwe ${currentYear}.`;
+
+const magnifier = document.getElementById('search');
+const searchBarContainer = document.getElementById('search-bar-container');
+
+magnifier.addEventListener('click', () => {
+  searchBarContainer.classList.toggle('active');
+});
